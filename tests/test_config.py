fc16d4a32aad92e36f3505ff9a1d0cf9e7492681
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2026-07-29 12:31:05 -0400

    Fix test
    Signed-off-by: Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>

--- tests/test_config.py
+++ tests/test_config.py
@@ -69,7 +69,7 @@ def test_missing_required_field_raises() -> None:
 
 
 def test_instances_key_not_a_list_raises() -> None:
-    """Test that the 'instances' YAML key being a non-list raises ValueError."""
+    """Test that the 'instances' YAML key being a non-list raises TypeError."""
     path = _write_config("""
 account_id: "acc-1"
 plan: "internal"
@@ -77,7 +77,7 @@ minimum_allocation_seconds: 60
 instances: "not-a-list"
 """)
     try:
-        with pytest.raises(ValueError, match="instances"):
+        with pytest.raises(TypeError, match="instances"):
             ConfigParser(path)
     finally:
         os.unlink(path)
